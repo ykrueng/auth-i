bea4ea6c804b6ae6460c735207e6d8386ded7719
ykrueng <yasirah.krueng@gmail.com>
2019-01-21 16:38:32 -0600

    add client React app

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Segment, Header } from 'semantic-ui-react';
+
+const Home = () => {
+  return (
+    <Segment>
+      <Header textAlign="center" as="h1">Welcome To Auth - I</Header>
+    </Segment>
+   );
+}
+ 
+export default Home;
