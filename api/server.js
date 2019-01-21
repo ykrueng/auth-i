917d93418c3190343e8d180d24b84fa06f7e3237
ykrueng <yasirah.krueng@gmail.com>
2019-01-21 11:11:11 -0600

    add POST /api/register with hashed password

--- api/server.js
+++ api/server.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const configMdlware = require("./config/middleware");
+const db = require("../data/helpers/userDb");
 
 const server = express();
 configMdlware(server);
 
 server.get("/", (req, res) => {
   res.send("Welcome to Auth-i Project");
+});
+
+server.post("/register", async (req, res) => {
+  const user = req.body;
+
+  if (!user || !user.username || !user.password) {
+    res.status(400).send("Invalid username or password.");
+  }
+
+  user.password = bcrypt.hashSync(user.password, 10);
+
+  try {
+    const userId = await db.register(user);
+    if (userId.length) {
+      res.status(201).json({ id: userId[0] });
+    } else {
+      res.status(500).send("Failed to register.");
+    }
+  } catch {
+    res.status(500).send("Failed to register.");
+  }
 });
 
 module.exports = server;
